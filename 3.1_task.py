1c3ad8f8494ce4a8d09e5789a7111f6874d79af7
Myroslava <myroslava.skorobagatko@gmail.com>
2023-01-29 01:23:32 +0200

    1st task

---
+++ 3.1_task.py
@@ -0,0 +1,5 @@
+my_elements = [1, 2, 3, 4, 5, 6, 7, 8]
+for index in range(0, len(my_elements), 2):
+    print([index, my_elements[index]])
+for index in range(1, len(my_elements), 2):
+    print([index, my_elements[index]])
