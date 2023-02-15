9d9564c2109518aa921411b75c5e188d4976f4a2
Myroslava <myroslava.skorobagatko@gmail.com>
2023-02-15 22:11:44 +0200

    3rd task corrections

--- 5.3_task.py
+++ 5.3_task.py
@@ -1,12 +1,17 @@
 import os
+import re
+
 os.chdir("./directory/for/file")
 with open ('text.txt', 'r') as file:
     result = file.read()
+letters = re.findall(r'[A-z]', result)
+letters_str = "".join(letters)
+print(letters_str)
 all_freq = {}
-for i in result:
-    if i in all_freq:
-        all_freq[i] += 1
+for letter in letters_str:
+    if letter in all_freq:
+        all_freq[letter] += 1
     else:
-        all_freq[i] = 1
+        all_freq[letter] = 1
 
 print(str(all_freq))
