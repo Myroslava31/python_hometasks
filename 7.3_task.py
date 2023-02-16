958dc602fb2f4c189603469cf7e6d6a8550b3429
Myroslava <myroslava.skorobagatko@gmail.com>
2023-02-17 00:02:15 +0200

    the third task

--- 7.3_task.py
+++ 7.3_task.py
@@ -3,17 +3,18 @@ from typing import Callable
 random_int = [2, 94, 34, 7, 111]
 random_float = [4.6, 7.1, 0.8, 37.4, 62.9]
 random_str = ['book', 'table', 'coffee', 'phone']
-new_map_list =[]
+
 def some_sum_func(item: (int, float, str)) -> (int, float, str):
     return item + item
 
-def map_function(callback: Callable, iterable: list) -> (int, float, str):
+def map_function(callback: Callable, iterable: list) -> list:
+    map_list = []
     for item in iterable:
-        new_map_list.append(some_sum_func(item))
-    return
+        if callback(item):
+            map_list.append(some_sum_func(item))
+    return map_list
 
 if __name__ == '__main__':
-    map_function(some_sum_func, random_int)
-    map_function(some_sum_func, random_float)
-    map_function(some_sum_func, random_str)
-    print(new_map_list)
+    print(map_function(some_sum_func, random_int))
+    print(map_function(some_sum_func, random_float))
+    print(map_function(some_sum_func, random_str))
