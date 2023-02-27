ff6c40b592a1c53825f22303d6387911aaeff711
Myroslava <myroslava.skorobagatko@gmail.com>
2023-02-27 21:05:01 +0200

    the first task

--- homework_8/8.1_task.py
+++ homework_8/8.1_task.py
@@ -1,26 +1,17 @@
-def func_name_decorator(name: str):
-    """DEC doc"""
+def func_name_decorator(func):
+    def inner(number1: int, number2: int):
+        return (f'{func.__name__}\n{func(number1, number2)}')
+    return inner
 
-    def middle_wraper(func):
-        """Middle wraper"""
-
-        def inner(number1: int, number2: int):
-            return f'__{name}__\n{func(number1, number2)}'
-
-        return inner
-
-    return middle_wraper
-
-
-@func_name_decorator('summation')
-def summ(number1: int, number2: int) -> int:
+@func_name_decorator
+def summation(number1: int, number2: int) -> int:
     return number1 + number2
 
 
-@func_name_decorator('multiplication')
-def multi(number1: int, number2: int) -> int:
+@func_name_decorator
+def multiplication(number1: int, number2: int) -> int:
     return number1 * number2
 
 if __name__ == '__main__':
-    print(summ(3, 6))
-    print(multi(3, 6))
+    print(summation(3, 6))
+    print(multiplication(3, 6))
