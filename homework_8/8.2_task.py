d2fdc30f09b37386bd43c929b284d0a2fac6f039
Myroslava <myroslava.skorobagatko@gmail.com>
2023-02-27 18:35:27 +0200

    the second task

--- homework_8/8.2_task.py
+++ homework_8/8.2_task.py
@@ -1,10 +1,10 @@
-def my_gen(first:int, last: int):
+def square_of_even(first:int, last: int):
     for number in range(first, last +1):
         if number % 2 == 0:
             yield number ** 2
 
 
 if __name__ == '__main__':
-    generator = my_gen(0, 1000)
+    generator = square_of_even(0, 1000000000)
     for result in generator:
         print(result)
