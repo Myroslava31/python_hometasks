07e523b27a73c220c804d02faac3444d427952dc
Myroslava <myroslava.skorobagatko@gmail.com>
2023-03-09 01:09:44 +0200

    Diprotodontia class

---
+++ homework_10/Diprotodontia.py
@@ -0,0 +1,13 @@
+from homework_10.Marsupial import Marsupial
+
+class Diprotodontia(Marsupial):
+    def __init__(self):
+        super().__init__()
+        self._food = 'herbivores and omnivores'
+        self._place = 'Australasia and Wallacea'
+
+    def eat(self):
+        return f'We are {self._food}'
+
+    def live(self):
+        return f'We live in {self._place}'
